18e8eddf2f180935061fd7463f731852431779ec
Eyob Kefale <97896057+eyob-kefale@users.noreply.github.com>
2024-02-13 22:08:30 +0300

    Update Login.jsx

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
     try {
-      const res = await axios.post("/auth/login", {
+      const res = await axios.post("https://blog-post-alx-api.vercel.app/auth/login", {
         userName: userRef.current.value,
         password: passwordRef.current.value,
       });
